ab7fe9cdc541c48eb0d002f764527bc3585f1ccd
Manas Ranjan Pati <iammanas24@gmail.com>
2021-11-17 16:43:26 +0530

    Added first and second project

---
+++ Part3BasicVueJS/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'Finished my course and learned the Vue!',
+            vueLink: 'https://www.w3schools.com/'
+            
+        }
+    },
+})
+
+app.mount('#user-goal'); 
